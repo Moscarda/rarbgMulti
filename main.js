83d2c1badca37ddb0e0e97b190de4688a8eaa239
Hank Yang <yrstyh@gmail.com>
2017-07-02 11:00:30 +0800

    update for new url

--- main.js
+++ main.js
@@ -2,25 +2,17 @@ var getI18N = chrome.i18n.getMessage;
 var body = $("body");
 var magnetIcon = "https://dyncdn.me/static/20/img/magnet.gif";
 var torrentIcon = "https://dyncdn.me/static/20/img/16x16/download.png";
-$("tr.lista2").each(function (){
-	var td = $(this).find("td").eq(1);
-	td.prepend("<input style='height: 13px;' class='rarbgcheck' type='checkbox'>")
-		.find("a").eq(0).after("<img title='" + getI18N("downloadTorrents") + "' class='momane_icon momane_torrent' src='" + torrentIcon + "'>" +
-		"<img title='" + getI18N("copyMagnet") + "'  class='momane_icon momane_magnet' src='" + magnetIcon + "'>");
-});
+addControls();
 
-$(".lista2 td:nth-child(2) a").click(function (e){
-	if (e.ctrlKey) {
-		e.preventDefault();
-		downloadTorrents($(this).prev(), false);
-	}
-});
 
-$(".momane_torrent").click(function (e){
+$("body").on("click", ".momane_torrent", function(){
 	downloadTorrents($(this).prev().prev(), true);
-});
-$(".momane_magnet").click(function (e){
+}).on("click", ".momane_magnet", function(){
 	downloadTorrents($(this).prev().prev().prev(), false, true, true);
+}).on("click", ".momane_expandall", function () {
+	$(this).parent().next().find(".tvshowClick").each(function () {
+		$(this)[0].click();
+    });
 });
 
 body.append("<div class='momane_wrapper'>" +
@@ -40,14 +32,16 @@ $("#searchinput").parents("tr").eq(0).after("<tr class='momane_searchOpt'><td>" 
 	"<label><input type='radio' name='searchOpt' id='seeders_DESC'>" + getI18N("seedersDESC") + "</label>" +
 	"<label><input type='radio' name='searchOpt' id='leechers_DESC'>" + getI18N("leechersDESC") + "</label>" +
 	"</td></tr>");
-$(".rarbgcheckall").click(function (){
+
+$("h1.black").append(`<button class='momane_expandall'>${getI18N("expandAll")}</button>`);
+$(".rarbgcheckall").click(function(){
 	$(".rarbgcheck").prop("checked", true);
 });
 
-$(".rarbguncheck").click(function (){
+$(".rarbguncheck").click(function(){
 	$(".rarbgcheck").prop("checked", false);
 });
-$(".rarbgdownload").click(function (){
+$(".rarbgdownload").click(function(){
 	var checked = $(".rarbgcheck:checked"),
 		downloadTorrent = $("#downloadTorrent").is(":checked");
 	downloadMagnet = $("#downloadMagnet").is(":checked");
@@ -55,7 +49,7 @@ $(".rarbgdownload").click(function (){
 
 });
 
-$("#searchTorrent").find("button.btn-primary").click(function (e){
+$("#searchTorrent").find("button.btn-primary").click(function(e){
 	if ($("input[name='searchOpt']").is(":checked")) {
 		e.preventDefault();
 		var url = "https://rarbg.to/torrents.php?search=";
@@ -65,7 +59,7 @@ $("#searchTorrent").find("button.btn-primary").click(function (e){
 		var officialOpt = $(".inputadvscat:checked");
 		if (officialOpt.length !== 0) {
 			var officialOpts = "";
-			officialOpt.each(function (i, opt){
+			officialOpt.each(function(i, opt){
 				var that = $(this);
 				officialOpts += "&" + encodeURI(that.attr("name")) + "=" + that.attr("value");
 			});
@@ -75,18 +69,34 @@ $("#searchTorrent").find("button.btn-primary").click(function (e){
 	}
 });
 
+var target = document.querySelector(".lista-rounded");
+var observer = new MutationObserver(function(mutations){
+	mutations.forEach(function(mutation){
+		if (mutation.removedNodes.length > 0) {
+			addControls($(mutation.target));
+		}
+		console.log(mutation);
+	});
+
+});
+
+var config = {childList:true, subtree:true};
+
+observer.observe(target, config);
+
+
 function downloadTorrents(sel, downloadTorrent, downloadMagnet, copy){
 	var urls = [];
-	sel.each(function (){
+	sel.each(function(){
 		var that = $(this);
 		urls.push("https://rarbg.to" + that.next().attr("href"));
 	});
 	var requests = [], magnites = [];
-	$.each(urls, function (i, url){
+	$.each(urls, function(i, url){
 		requests.push($.ajax({
 			url:url,
 			i:i,
-			success:function (data){
+			success:function(data){
 				var html = $($.parseHTML(data));
 				var a = html.find("a[onmouseover^='return overlib']").eq(0),
 					link = a.attr("href");
@@ -99,7 +109,18 @@ function downloadTorrents(sel, downloadTorrent, downloadMagnet, copy){
 		}));
 	});
 	if (downloadMagnet) {
-		$.when.apply($, requests).then(function (){
+		$.when.apply($, requests).then(function(){
+			magnites.sort(function(x, y){
+				x = x.replace(/.*&dn=/, "");
+				y = y.replace(/.*&dn=/, "");
+				if(x>y){
+					return 1;
+				}else if(x<y){
+					return -1;
+				}else {
+					return 0;
+				}
+			});
 			var magStr = magnites.join("\t\n");
 			var fileUrl = makeTextFile(magStr);
 			if (!copy) {
@@ -109,7 +130,7 @@ function downloadTorrents(sel, downloadTorrent, downloadMagnet, copy){
 			} else {
 				copyTextToClipboard(magStr.trim());
 				$(".momane_notification").show().text(getI18N("magnetCopied"));
-				setTimeout(function (){
+				setTimeout(function(){
 					$(".momane_notification").fadeOut();
 				}, 2000);
 			}
@@ -117,9 +138,29 @@ function downloadTorrents(sel, downloadTorrent, downloadMagnet, copy){
 	}
 }
 
+function addControls(sel){
+	var downloadControlStr = "tr.lista2";
+	var ctrlDownloadStr = ".lista2 td:nth-child(2) a";
+	var downloadControl = sel ? sel.find(downloadControlStr):$(downloadControlStr);
+	var ctrlDownload = sel ? sel.find(ctrlDownloadStr):$(ctrlDownloadStr);
+	downloadControl.each(function(){
+		var td = $(this).find("td").eq(1);
+		td.prepend("<input style='height: 13px;' class='rarbgcheck' type='checkbox'>")
+			.find("a").eq(0).after("<img title='" + getI18N("downloadTorrents") + "' class='momane_icon momane_torrent' src='" + torrentIcon + "'>" +
+			"<img title='" + getI18N("copyMagnet") + "'  class='momane_icon momane_magnet' src='" + magnetIcon + "'>");
+	});
 
-var textFile = null;
+	ctrlDownload.click(function(e){
+		if (e.ctrlKey) {
+			e.preventDefault();
+			downloadTorrents($(this).prev(), false);
+		}
+	});
+}
+
+
 function makeTextFile(text){
+	var textFile = null;
 	var data = new Blob([text], {type:'text/plain'});
 
 	// If we are replacing a previously generated file we need to
@@ -148,3 +189,4 @@ function copyTextToClipboard(text){
 	document.execCommand('copy', true);
 	copyFrom.remove();
 }
+
